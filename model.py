11979aa48c22f6beb8f62a1e467145dccd41fcd2
YaroslavRy <ya.ryndyk@gmail.com>
2020-10-09 02:54:34 +0300

    wip

--- model.py
+++ model.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, GRU, Input
 from tensorflow.keras.optimizers import Adam
@@ -17,33 +15,32 @@ def laod_model():
     return
 
 
-def build_model(timesteps, lr, gru_units=32, fc_units=16):
+def build_model(timesteps, lr, gru_units, fc_units, output_shape):
     K.clear_session()
     model = Sequential()
     model.add(GRU(units=gru_units, input_shape=(timesteps, 1,), return_sequences=False))
     model.add(Dense(units=fc_units, activation='relu'))
-    model.add(Dense(units=3, activation='softmax'))
+    model.add(Dense(units=output_shape, activation='softmax'))
     model.compile(optimizer=Adam(lr), loss='sparse_categorical_crossentropy')
     return model
 
 
+# TODO: move to config
 lr = 0.001
 batch_size = 32
-input_dim = 100    # represents a sequence length.
+input_dim = 100  # represents a sequence length.
 hidden_dim = 256
-output_dim = 1
+output_shape = 3
 n_layers = 2
 N = 1000
-
 
 X = np.random.normal(size=(N, input_dim, 1))
 y = np.random.randint(0, 3, size=(N, 1))
 
 plt.plot(X.flatten())
+plt.show()
 
-model = build_model(input_dim, lr=lr, gru_units=32, fc_units=16)
+model = build_model(input_dim, lr=lr, gru_units=32, fc_units=16, output_shape=output_shape)
 model.summary()
 
-model.fit(X, y, epochs=2)
-
-
+model.fit(X, y, epochs=1)
