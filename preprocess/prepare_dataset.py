ac04a8a5199a5894afda936de36da245d71f9f1b
YaroslavRy <ya.ryndyk@gmail.com>
2020-11-05 02:54:48 +0200

    wip

--- preprocess/prepare_dataset.py
+++ preprocess/prepare_dataset.py
@@ -17,19 +17,21 @@ plt.style.use('seaborn')
 import os
 from voice_encoder import VoiceEncoder
 plt.style.use('seaborn')
-from utils import load_pickle
+from utils import load_pickle, save_pickle
 from scipy import signal
 from hparams import sampling_rate
+from tqdm import tqdm
 
 
-PATH_TO_SAVE = './audio_data/combined/'
-AUDIO_PATH = '/Users/nemo/Downloads/DS_10283_2651/VCTK-Corpus/wav48/'
+COMBINED_UTTERS_PATH = './audio_data/combined/'
+PATH_TO_SAVE = './data/combined_embeddings/'
+
+# !mkdir './data/'
+# !mkdir './data/combined_embeddings/'
+
 sr = sampling_rate
 
-print(os.listdir(AUDIO_PATH + 'p301')) 
-
-filepath = AUDIO_PATH + 'p304/p304_232.wav'
-a = preprocess_wav(filepath, sampling_rate=sr)
+print(os.listdir(COMBINED_UTTERS_PATH)) 
 
 filepath = './audio_data/combined/p228_2_11960'
 
@@ -37,7 +39,7 @@ wav, labels = load_pickle(filepath)
 
 play_wav_file(wav, fs=sr)
 
-print(a.shape[0]/sr)
+print(wav.shape[0]/sr)
 
 plot_spectrogram(wav)
 
@@ -47,41 +49,66 @@ _, cont_embeds, wav_splits = encoder.embed_utterance(wav, return_partials=True, 
 wav_splits
 
 
-embedds = []
-labels_emb = []
-slice_len = sr * 0.5
-prev_ind = 0
-for i in range(int(-np.floor(-wav.shape[0]/slice_len))):
-    curr_index = int(prev_ind + slice_len)
-    emb = encoder.embed_utterance(wav[prev_ind: curr_index], return_partials=False, rate=1.5)
-    embedds.append(emb)
-    labels_emb.append(int(np.median(labels[prev_ind: curr_index])))
-    prev_ind = curr_index
 
-plt.plot(np.array(embedds).flatten())
-
-plt.plot(embedds)
-
-mean_emb = np.mean(embedds, axis=1)
-plt.plot((mean_emb - np.min(mean_emb))/(np.max(mean_emb) - np.min(mean_emb)))
-plt.plot(labels_emb)
-
-
-for i in labels_emb:
-    plt.hist(i)
+def prepare_dataset(path_combined_utters, path_to_save, slice_len=0.1):
+    utters_list = os.listdir(path_combined_utters)
+    for filename in tqdm(utters_list):
+        wav, labels = load_pickle(path_combined_utters+filename)
+        embedds = []
+        labels_emb = []
+        slice_len *= sr
+        n_slices = int(-np.floor(-wav.shape[0]/slice_len))  # hack to floor to biggest
+        prev_ind = 0
+        for i in range(n_slices):
+            curr_index = int(prev_ind + slice_len)
+            emb = encoder.embed_utterance(wav[prev_ind: curr_index], return_partials=False, rate=1.5)
+            embedds.append(emb)
+            labels_emb.append(int(np.median(labels[prev_ind: curr_index])))
+            prev_ind = curr_index
+        save_pickle([embedds, labels_emb], path_to_save + filename + '.dat')
 
 
-f, t, Sxx = signal.spectrogram(wav, sr, )
-S_db = librosa.amplitude_to_db(np.abs(Sxx), ref=np.max)
-plt.pcolormesh(t, f, S_db, shading='nearest', cmap=plt.cm.viridis)
-plt.ylabel('Frequency [Hz]')
-plt.xlabel('Time [sec]')
-plt.show()
+prepare_dataset(path_combined_utters=COMBINED_UTTERS_PATH, path_to_save=PATH_TO_SAVE, slice_len=0.5)
 
 
-plt.specgram(wav, Fs=sr, cmap=plt.cm.inferno)
-plt.grid()
-
-
-plt.plot(labels)
-
+# =============================================================================
+# 
+# embedds = []
+# labels_emb = []
+# slice_len = sr * 0.5
+# prev_ind = 0
+# for i in range(int(-np.floor(-wav.shape[0]/slice_len))):
+#     curr_index = int(prev_ind + slice_len)
+#     emb = encoder.embed_utterance(wav[prev_ind: curr_index], return_partials=False, rate=1.5)
+#     embedds.append(emb)
+#     labels_emb.append(int(np.median(labels[prev_ind: curr_index])))
+#     prev_ind = curr_index
+# 
+# plt.plot(np.array(embedds).flatten())
+# 
+# plt.plot(embedds)
+# 
+# 
+# mean_emb = np.std(embedds, axis=1)
+# plt.plot((mean_emb - np.min(mean_emb))/(np.max(mean_emb) - np.min(mean_emb)))
+# plt.plot(labels_emb)
+# 
+# for i in labels_emb:
+#     plt.hist(i)
+# 
+# 
+# f, t, Sxx = signal.spectrogram(wav, sr, )
+# S_db = librosa.amplitude_to_db(np.abs(Sxx), ref=np.max)
+# plt.pcolormesh(t, f, S_db, shading='nearest', cmap=plt.cm.viridis)
+# plt.ylabel('Frequency [Hz]')
+# plt.xlabel('Time [sec]')
+# plt.show()
+# 
+# 
+# plt.specgram(wav, Fs=sr, cmap=plt.cm.inferno)
+# plt.grid()
+# 
+# 
+# plt.plot(labels)
+# 
+# =============================================================================
